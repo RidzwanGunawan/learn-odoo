05054b6e4b6705bb4e7e62b14d9a4bf781615572
Ridzwan Gunawan <142468544+RidzwanGunawan@users.noreply.github.com>
2025-07-30 08:55:51 +0700

    Delete rental_management directory

--- rental_management/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import rental_product
-from . import rental_pricing
-from . import rental_order
-from . import rental_order_line
-from . import rental_schedule
-from . import product_template
